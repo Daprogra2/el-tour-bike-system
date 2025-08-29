4ee0071dd3f07d7ce0235c09071beef2accb3ecc
Ernie <sonoranpc@gmail.com>
2025-08-29 10:13:14 -0700

    correct imports and keys 5th

--- public/map.js
+++ public/map.js
@@ -1,5 +1,9 @@
 // map.js
-import L from 'leaflet';
+
+const map = L.map('map').setView([32.2217, -110.9265], 13);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; OpenStreetMap contributors'
+}).addTo(map);
 
 function initMap(containerId) {
   const map = L.map(containerId).setView([32.2217, -110.9265], 13);
