c1cd471cfff36276776f840f1fa4dbdaba7687e0
Ernie <sonoranpc@gmail.com>
2025-09-16 11:57:50 -0700

    Version 2.0

--- public/form.js
+++
@@ -1,9 +0,0 @@
-// form.js
-async function submitMessage(name, message) {
-  const res = await fetch('/api/messages', {
-    method: 'POST',
-    body: JSON.stringify({ name, message }),
-    headers: { 'Content-Type': 'application/json' },
-  });
-  return res.ok;
-}
